47c8b5a6d6c1b38580e99c37574dc98f269c2c39
Nahush Naik <nahushmnaik@gmail.com>
2015-12-09 23:47:32 +0530

    Commented Rot13

--- src/Rot13.java
+++ src/Rot13.java
@@ -1,26 +1,33 @@
 import java.io.*;
 public class Rot13 {
 
+	
+	/*method used to encrypt and decrypt the text file (same fuction performs
+	 * both the tasks because the alphabet is just rotated by 13 places)*/ 
 	void encrypt(String file) throws IOException
 	{
 		char c;
+		/*StringBuffer object used for fast and more memory efficient 
+		concatination within loop*/
 		StringBuilder sb = new StringBuilder();
 		BufferedReader br = new BufferedReader(new FileReader(file));
 		String line = br.readLine();
+		//read the text file line by line
 		while (line != null) {
 			for(int i = 0; i < line.length(); i++)
 			{
 				c = line.charAt(i);
+				//check whether the character is from the first 13 alphabets  
 				if(c >= 'a' && c <= 'm')
-					c += 13;
-				else if(c >= 'm' && c <= 'z')
-					c -= 13;
+					c += 13;	//shift right by 13 places
+				else if(c >= 'm' && c <= 'z')	//check whether the character is from the last 13 alphabets
+					c -= 13;	//shift left by 13 places
 				else if(c >= 'A' && c <= 'M')
 					c += 13;
 				else if(c >= 'M' && c <= 'Z')
 					c -= 13;
-				sb.append(c);
-			}
+				sb.append(c);	//if character does not belong to the set 
+			}					//of alphabets it is directly appended to final string
 			sb.append("\n");
             line = br.readLine();
 		}
