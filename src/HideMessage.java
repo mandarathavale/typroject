c24bcc9fe20c350d15831e8994f9c255b0d94781
Nahush Naik <nahushmnaik@gmail.com>
2015-12-17 20:58:19 +0530

    Add bit to blue's binary (needs to be fixed)

--- src/HideMessage.java
+++ src/HideMessage.java
@@ -7,19 +7,43 @@ public class HideMessage {
 
 	public static void main(String[] args) throws IOException {
 		char temp;
-		String binaryString, binaryBlue;
+		StringBuilder binaryBlue = new StringBuilder();
+		String binaryString;
 		File image = new File(args[0]);
 		System.out.println(args[0]);
 		BufferedImage bimg = ImageIO.read(image);
-		int pixel[][] = new int[bimg.getWidth()][bimg.getHeight()];
+		String pixel[][] = new String[bimg.getWidth()][bimg.getHeight()];
 		binaryString = ASCII.encode(args[1]);
+		int a = 0;
+		System.out.println("Before bit adding: ");
 		for(int i=0; i<bimg.getWidth(); i++)
 		{
 			for(int j=0; j<bimg.getHeight(); j++)
 			{
-				System.out.println(Integer.toBinaryString(bimg.getRGB(i, j) & 0xff));
+				pixel[i][j] = Integer.toBinaryString(bimg.getRGB(i, j) & 0xff);
+				System.out.println(pixel[i][j]);
+			}
+		}
+		label:
+		for(int i=0; i<bimg.getWidth(); i++)
+		{
+			for(int j=0; j<bimg.getHeight(); j++)
+			{
+				binaryBlue.append(Integer.toBinaryString(bimg.getRGB(i, j) & 0xff));
+				binaryBlue.setCharAt(binaryBlue.length()-1, binaryString.charAt(a));
+				pixel[i][j] = binaryBlue.toString();
+				a++;
+				if(a <= binaryString.length())
+					break label;
+			}
+		}
+		System.out.println("After bit adding: ");
+		for(int i=0; i<bimg.getWidth(); i++)
+		{
+			for(int j=0; j<bimg.getHeight(); j++)
+			{
+				System.out.println(pixel[i][j]);
 			}
 		}
 	}
-
 }
