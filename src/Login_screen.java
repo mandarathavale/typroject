83bb9e382a73d35ad30c74710f9d8d7b5b100902
Nahush Naik <nahushmnaik@gmail.com>
2016-01-01 12:50:46 +0530

    Working enter for login

--- src/Login_screen.java
+++ src/Login_screen.java
@@ -1,6 +1,9 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.font.TextAttribute;
@@ -42,7 +45,7 @@ public class Login_screen {
 		sign = new JLabel("Create new account ->");
 		forg = new JLabel("Forgot your password?");
 		useriderror_label = new JLabel("*");
-		passerror_label = new JLabel("*");	
+		passerror_label = new JLabel("*");
 		
 		pss.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -173,6 +176,7 @@ public class Login_screen {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
+		frame.getRootPane().setDefaultButton(log);
 	}
 
 	public static void main(String[] args) throws IOException {
